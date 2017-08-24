f40ffc7efc11e423f42e9ff5313c98630ccfcd9e
carla <carlacfw@gmail.com>
2017-08-24 15:46:57 +1200

    updated users table

--- migrations/20160827154101_users.js
+++ migrations/20160827154101_users.js
@@ -1,7 +1,10 @@
 exports.up = knex => knex.schema.createTable('users', table => {
   table.increments('id').primary()
+  table.string('name')
   table.string('username')
   table.binary('hash')
+  table.string('email')
+  table.string('phone-number')
 })
 
 exports.down = knex => knex.schema.dropTable('users')
