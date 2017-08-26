118d4aa958a45bb543d9c6d0bf612f29bd6fef88
gabriel <777canaan@gmail.com>
2017-08-26 17:49:23 +1200

    11 passing tests 2 fail

--- client/index.js
+++ client/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import thunk from 'redux-thunk'
 
-import reducers from './reducers'
 import App from './components/App'
 
-const store = createStore(reducers, compose(
-  applyMiddleware(thunk)))
+import store from './store'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
