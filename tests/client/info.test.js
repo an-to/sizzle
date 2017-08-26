caf00cba8be069116a780c1f27d0f574bda9fafe
gabriel <777canaan@gmail.com>
2017-08-26 17:07:55 +1200

    package json babel test additions

--- tests/client/info.test.js
+++ tests/client/info.test.js
@@ -1,8 +1,9 @@
+import './setup-dom'
 import React from 'react'
 import test from 'ava'
 import { shallow, mount } from 'enzyme'
 
-import Info from '../../../client/components/Info'
+import Info from '../../client/components/Info'
 
 test('Info showing', t => {
   const wrapper = shallow(<Info />)
