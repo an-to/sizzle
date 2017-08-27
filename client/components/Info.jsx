e0d6078e9fba323668a7e47518f7e253afe1b18d
carla <carlacfw@gmail.com>
2017-08-28 09:36:25 +1200

    component info updated

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Grid,Row,Col,Input,Container} from 'react-bootstrap'
+import jump from 'jump.js'
 
   const Info = () => {
     return (
@@ -10,6 +11,7 @@ import {Grid,Row,Col,Input,Container} from 'react-bootstrap'
             <Col xs={1} md={4}></Col>
             <Col xs={4} md={4}>
               <h1>Info</h1>
+              <div className='info'>
               <p>Sizzle was created with the aim of encouraging people not to
                 waste food and make it an action where you provide some resource
                 between food, skill or a kitchen to prepare the food that is made available.
@@ -19,10 +21,11 @@ import {Grid,Row,Col,Input,Container} from 'react-bootstrap'
                 can choose between consuming, donating to some institution, selling, it will
                 really be the choice of you. We are grateful for your
                 help in avoiding food waste, thank you.</p>
-              <br>
+
                 <p>Love,</p>
-                <br>
-                  Team Sizzle.
+
+                  <p>Team Sizzle.</p>
+                  </div>
             </Col>
             <Col xs={1} md={4}></Col>
           </Row>
