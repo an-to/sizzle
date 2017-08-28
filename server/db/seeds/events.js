0048a98d735a747e36cfc8917c8056ddd677e0f4
an-to <an-to@users.noreply.github.com>
2017-08-28 12:24:25 +1200

    events seeds date updated

--- server/db/seeds/events.js
+++ server/db/seeds/events.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('events').insert([
-        {id: 1, date: '27.08.2018', is_am: 't', description: 'Newtown School community get together', location_id: '3' },
-        {id: 2, date: '08.09.2018', is_am: 'f', description: 'cancer society fundraiser', location_id: '1' },
-        {id: 3, date: '16.09.2018', is_am: 't', description: 'kaibosh fundraiser', location_id: '2' }
+        {id: 1, date: new Date(2017,08,27), is_am: 't', description: 'Newtown School community get together', location_id: '3' },
+        {id: 2, date: new Date(2017,09,02), is_am: 'f', description: 'cancer society fundraiser', location_id: '1' },
+        {id: 3, date: new Date(2017,09,08), is_am: 't', description: 'kaibosh fundraiser', location_id: '2' }
       ]);
     });
 };
