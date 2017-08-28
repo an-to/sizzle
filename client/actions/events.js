c6a8494b287d6ea53a273508e15e804c60e22a3f
Chris Chou <chhschou@gmail.com>
2017-08-28 15:46:09 +1200

    update eventpage.jsx to latest

---
+++ client/actions/events.js
@@ -0,0 +1,45 @@
+import request from '../utils/api'
+
+export const receiveEvents = (events) => {
+  return {type: 'GET_EVENTS', events}
+}
+
+export const addEvent = (event) => {
+  return {type: 'ADD_EVENT', event}
+}
+
+export const removeEvents = (event) => {
+  return {type: 'REMOVE_EVENT', event}
+}
+
+export function getEvents() {
+  return (dispatch) => {
+    request('get','/events')
+    .then(res => {
+      dispatch(receiveEvents(res.body))
+    })
+    .catch(err => console.error(err))
+  }
+}
+
+export function createEvent(event) {
+  return (dispatch) => {
+    request('post','/events')
+    .send(event)
+    .then(res => {
+      dispatch(addEvent(res.body))
+    })
+    .catch(err => console.error(err))
+  }
+}
+
+export function removeEvent(event) {
+  return (dispatch) => {
+    request('delete',`/events/${event.id}`)
+    .send(event)
+    .then(res => {
+      dispatch(removeEvent(res.body))
+    })
+    .catch(err => console.error(err))
+  }
+}
--- client/container/EventPage.jsx
+++ client/container/EventPage.jsx
@@ -1,27 +1,79 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Grid, Row, Col, Input, Container } from 'react-bootstrap'
+import moment from 'moment'
+
+import { createEvent } from '../actions/events'
 import Logo from '../components/Logo'
-import Quote from '../components/Quote'
 
-
-
-
-export default class EventPage extends React.Component {
+export class EventPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      term: ''
+      title: '',
+      description: '',
+      host: null,
+      date: new Date(),
+      is_am: true,
     }
   }
+
+  onChange(e) {
+    let value = e.target.value
+    if (e.target.name === 'date') 
+      value = new Date(value) 
+
+    this.setState({ [e.target.name]: value })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.dispatch(createEvent(this.state))
+  }
+
   render() {
+    let dateString = moment(this.state.date).format('DD/MMM/YYYY')
     return (
-      <div>
+      <div className='event'>
         <Logo />
-        <Quote />
-        <Link to='/'>
-           <button type="button" className="btn btn-primary">Home</button>
-         </Link>
+        <Grid>
+          <h2>Start a sizzle</h2>
+          <form onSubmit={this.onSubmit.bind(this)}>
+          <Row>
+            <input type='text' name='title' placeholder='Please enter title' onChange={this.onChange.bind(this)} />
+          </Row>
+          <Row>
+            <input type='text' name='description' placeholder='Please enter description' onChange={this.onChange.bind(this)} />
+          </Row>
+          <Row>
+            <input type="text" name='date' placeholder='DD/MMM/YYYY' onChange={this.onChange.bind(this)}/>
+            <select name='is_am' onChange={this.onChange.bind(this)}>
+              <option value='true'>AM</option>
+              <option value='false'>PM</option>
+            </select>
+          </Row>
+          <Row>
+            <button type='submit' >Sizzle!</button>
+          </Row>
+          </form>
+          <Row>
+            <Link to='/'>
+              <button type="button" className="btn btn-primary">Home</button>
+            </Link>
+          </Row>
+        </Grid>
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    ingredients: state.ingredients,
+    locations: state.locations,
+    skills: state.skills
+  }
+}
+
+export default connect(mapStateToProps)(EventPage) 
